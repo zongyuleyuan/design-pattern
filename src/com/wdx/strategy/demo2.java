2e4547a940a81c7e680c4e406666f5b5b8c5aa07
zongyuleyuan <zongyuleyuan@gmail.com>
2016-11-29 16:54:46 +0800

    修改了demo2

--- src/com/wdx/strategy/demo2.java
+++ src/com/wdx/strategy/demo2.java
@@ -2,6 +2,7 @@ package com.wdx.strategy;
 public class demo2 {
     public static void main(String[] args) {
         System.out.println("load github");
+        System.out.println("测试成功");
     }
 
 }

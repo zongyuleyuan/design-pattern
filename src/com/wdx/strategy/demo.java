c69a0041fed3bc6a076e35db61b95d9a9d1b0c03
zongyuleyuan <zongyuleyuan@gmail.com>
2016-11-29 16:37:34 +0800

    修改了demo

---
+++ src/com/wdx/strategy/demo.java
@@ -0,0 +1,10 @@
+package com.wdx.strategy;
+
+public class demo {
+    public static void main(String[] args){
+        System.out.println("helloword");
+        System.out.println("github");
+    }
+
+
+}
